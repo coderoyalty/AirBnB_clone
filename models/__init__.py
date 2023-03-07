d7c3d772fa5a22c1afd301be2f6c2630486aabd3
Royalty <akannie33@gmail.com>
2023-03-07 14:49:24 +0100

    created FileStorage, updated BaseModel

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""
+__init__ for models module
+"""
+from models.engine.file_storage import FileStorage
+
+storage = FileStorage()
+storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@
 """
 from datetime import datetime
 import uuid
+from models import storage
 
 
 class BaseModel:
@@ -36,6 +37,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
         else:
             for key, value in kwargs.items():
                 if key in ("created_at", "updated_at"):
@@ -52,6 +54,7 @@ class BaseModel:
         """
 
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """
